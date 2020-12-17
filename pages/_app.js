cc93866ecfdf700f94bd69fc3a9343d3d4513b6c
Carlos Henrique Rabelo de Oliveira <carloshenriquerabelodeoliveira@MBP-de-Carlos.box>
2020-12-17 11:59:40 -0300

    chore: add absolute import

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Layout from '../layout'
+import React from "react";
+import PropTypes from "prop-types";
+import Layout from "layout";
 
-import Head from 'next/head'
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => (
   <>
     <Head>
       <title>WIP</title>
-      <link rel='icon' href='/favicon.ico' />
+      <link rel="icon" href="/favicon.ico" />
     </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
   </>
-)
+);
 
-App.propTypes = { Component: PropTypes.any, pageProps: PropTypes.any }
+App.propTypes = { Component: PropTypes.any, pageProps: PropTypes.any };
 
-export default App
+export default App;
